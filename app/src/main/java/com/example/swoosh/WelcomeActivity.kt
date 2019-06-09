1c43ad8ddd7724c5676f99725f7409a4f7a67f3d
VEDA <ralphluganob@gmail.com>
2019-06-09 12:49:58 +0800

    League Activity initial

--- app/src/main/java/com/example/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/swoosh/WelcomeActivity.kt
@@ -1,12 +1,26 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    // bundles are data objects that contain key value pairs
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedBtn.setOnClickListener {
+
+            // an intent is an object that lets you interact with other applications or your own.
+            /* implicit intent = specifies an action type and sends it to android system to respond to an intent (if there
+                                  are too many apps, it will leave you with the choices of compatible apps*/
+            // explicit intent = these are specific intent to open a specific app
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
+
     }
 }
