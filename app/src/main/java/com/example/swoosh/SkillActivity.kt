f100c34b725a10df8a05ebc8503d335cf268c1f7
VEDA <ralphluganob@gmail.com>
2019-06-09 16:26:44 +0800

    Lesson end

--- app/src/main/java/com/example/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 
 class SkillActivity : AppCompatActivity() {
 
+    var league = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
     }
 }
